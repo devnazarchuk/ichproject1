5f8ee9367da2ece5caaea7f5d6096fec985be918
devnazarchuk <devnazarchuk@gmail.com>
2024-11-10 15:17:48 +0100

    layout done

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ function toggleAccordion(id) {
     document.querySelector(`#${id}`)
         .classList.toggle('accordion-active')
 }
+
+const acceptButton = document.querySelector('.accept-button');
+const rejectButton = document.querySelector('.reject-button');
+const cookiesSection = document.querySelector('.cookies');
+
+
+function hideCookiesSection() {
+  cookiesSection.style.display = 'none';
+}
+
+
+acceptButton.addEventListener('click', hideCookiesSection);
+rejectButton.addEventListener('click', hideCookiesSection);
+
+if (localStorage.getItem('cookiesAccepted') === 'true') {
+    cookiesSection.style.display = 'none';
+  }
+  
+  acceptButton.addEventListener('click', () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    hideCookiesSection();
+  });
+  
+  rejectButton.addEventListener('click', () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    hideCookiesSection();
+  });
+  
